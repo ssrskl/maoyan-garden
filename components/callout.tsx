fa867f1be86a85449a8b5dc47baba7b421087048
ssrskl <1071352028@qq.com>
2025-12-19 23:12:07 +0800

    style(components): 调整 callout 组件的间距和样式
    优化 callout 组件的内外边距，使其更紧凑美观。同时调整代码块的字体大小和行高，改进整体阅读体验

--- components/callout.tsx
+++ components/callout.tsx
@@ -13,7 +13,7 @@ export function Callout({
   ...props
 }: CalloutProps) {
   return (
-    <div className="relative mb-12 mr-6">
+    <div className="relative pb-8 mr-6">
       <div className="absolute top-0 left-0 z-10">
         <div className="w-12 h-12 flex justify-center items-center">
           {type === "info" && <MdInfoOutline className="w-8 h-8 text-[#4433FF] drop-shadow-icon-bg-light dark:drop-shadow-icon-bg-dark" />}
@@ -21,7 +21,7 @@ export function Callout({
           {type === "danger" && <MdOutlineDangerous className="w-8 h-8 text-[#FF0000] drop-shadow-icon-bg-light dark:drop-shadow-icon-bg-dark" />}
         </div>
       </div>
-      <div className={cn("relative top-6 left-6  border-l-4 py-2 px-4 dark:bg-primary/10 rounded-md",
+      <div className={cn("relative top-6 left-6  border-l-4 py-1 px-4 dark:bg-primary/10 rounded-md",
         {
           "border-[#4433FF] bg-[#E8F1FC]": type === "info",
           "border-[#FF9900] bg-[#FFF3E0]": type === "warning",
